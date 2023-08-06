7098dbdea83923299bae5af5ca3cec85a8acd238
Khari Hopes <buildingkhari@yahoo.com>
2023-08-06 18:34:25 -0500

    fixed spelling

--- js/app.js
+++ js/app.js
@@ -5,14 +5,14 @@ let username = prompt("Hi, What is your name?");
 console.log("User's name", username);
 
 
-let middlename = prompt("How many middle namnes do you have?");
+let middlename = prompt("How many middle names do you have?");
 console.log("User's middle name amount", middlename);
 
 
 let answer = prompt("Does K'hari have four middle names?").toLowerCase();
    
 if (answer === "yes" || answer === "y") {
-alert("Good job " + username + ". The answer is indeed " + answer);
+alert("Good job " + username + ". K'hari does indeed have 4 middle names");
 } else {
 alert("Sorry K'hari does indeed have 4 middle names");
 }

ae199af563b2a2374c61ca6d03b8dd473828a2d5
Khari Hopes <buildingkhari@yahoo.com>
2023-08-07 19:36:03 -0500

    fixed java

--- js/app.js
+++ js/app.js
@@ -11,7 +11,9 @@ console.log("User's middle name amount", middlename);
 let answer = prompt("Does K'hari have four middle names?").toLowerCase();
    
 if (answer === "yes" || answer === "y") {
+// console.log("Good job " + username + ". K'hari does indeed have 4 middle names");
 alert("Good job " + username + ". K'hari does indeed have 4 middle names");
 } else {
+// console.log("Sorry K'hari does indeed have 4 middle names");
 alert("Sorry K'hari does indeed have 4 middle names");
 }

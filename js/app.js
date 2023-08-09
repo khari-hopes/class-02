31759113fad5d29abd7654dfe957094d21dfe55c
Khari Hopes <buildingkhari@yahoo.com>
2023-08-09 18:34:55 -0500

    added different questions

--- js/app.js
+++ js/app.js
@@ -1,19 +1,49 @@
 'use strict';
 
-let username = prompt("Hi, What is your name?");
+
+let username = prompt("Hi, What is your name");
 console.log("User's name", username);
 
+let myBirth = prompt("Was Khari born in Wisconsin").toLowerCase();
 
-let middlename = prompt("How many middle names do you have?");
-console.log("User's middle name amount", middlename);
-
-
-let answer = prompt("Does K'hari have four middle names?").toLowerCase();
-   
-if (answer === "yes" || answer === "y") {
-// console.log("Good job " + username + ". K'hari does indeed have 4 middle names");
-alert("Good job " + username + ". K'hari does indeed have 4 middle names");
+if (myBirth === 'yes' || myBirth === 'y') {
+  alert("Good job ' + username + '. K'hari was born in Wisconsin");
 } else {
-// console.log("Sorry K'hari does indeed have 4 middle names");
+  alert("No K'hari was indeed born in Wisconsin");
+}
+
+let middleName = prompt("Does K'hari have four middle names?").toLowerCase();
+   
+if (middleName === 'yes' || middleName === 'y') {
+alert("Good job ' + username + '. K'hari does indeed have 4 middle names");
+} else {
 alert("Sorry K'hari does indeed have 4 middle names");
 }
+
+let myLocation = prompt("Do I live in California").toLowerCase();
+
+if (myLocation === 'no' || myLocation === 'n') {
+  alert("Good job ' + username + '. K'hari lives in Louisiana!");
+  } else {
+  alert("Sorry K'hari doesn't live in California");
+  }
+
+
+let mySiblings = prompt("Does K'hari have seven siblings").toLowerCase();
+
+if (mySiblings === 'yes' || mySiblings === 'y') {
+  alert("Good job ' + username + '. K'hari does indeed have 7 siblings");
+  } else {
+  alert("Wrong K'hari does have 7 siblings");
+  }
+
+
+
+
+let score=
+
+userScore ++;
+console.log:("userScore:" userScore)
+
+"can you guess how many middle names i have?"
+

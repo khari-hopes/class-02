4107e75ee938af517ab9a8f950d181781410f9e4
Khari Hopes <buildingkhari@yahoo.com>
2023-08-09 19:20:28 -0500

    fixed questions error

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 "use strict";
 
-let username = prompt("Hi, What is your name");
+let username = prompt("Hi, What is your name?");
 console.log("User's name", username);
 
-let myBirth = prompt("Was Khari born in Wisconsin").toLowerCase();
+let myBirth = prompt("Was Khari born in Wisconsin?").toLowerCase();
 
 if (myBirth === 'yes' || myBirth === 'y') {
-  alert("Good job ' + username + '. K'hari was born in Wisconsin");
+  alert("Good job " + username + ". K'hari was born in Wisconsin");
 } else {
   alert("No K'hari was indeed born in Wisconsin");
 }
@@ -14,35 +14,32 @@ if (myBirth === 'yes' || myBirth === 'y') {
 let middleName = prompt("Does K'hari have four middle names?").toLowerCase();
    
 if (middleName === 'yes' || middleName === 'y') {
-alert("Good job ' + username + '. K'hari does indeed have 4 middle names");
+alert("Good job " + username + ". K'hari does indeed have 4 middle names");
 } else {
 alert("Sorry K'hari does indeed have 4 middle names");
 }
 
-let myLocation = prompt("Do I live in California").toLowerCase();
+let myLocation = prompt("Does K'hari live in California?").toLowerCase();
 
 if (myLocation === 'no' || myLocation === 'n') {
-  alert("Good job ' + username + '. K'hari lives in Louisiana!");
+  alert("Good job " + username + ". K'hari lives in Louisiana!");
   } else {
   alert("Sorry K'hari doesn't live in California");
   }
 
 
-let mySiblings = prompt("Does K'hari have seven siblings").toLowerCase();
+let mySiblings = prompt("Does K'hari have seven siblings?").toLowerCase();
 
 if (mySiblings === 'yes' || mySiblings === 'y') {
-  alert("Good job ' + username + '. K'hari does indeed have 7 siblings");
+  alert("Good job " + username + ". K'hari does indeed have 7 siblings");
   } else {
   alert("Wrong K'hari does have 7 siblings");
   }
 
+let guessQuestion = prompt("How many middle names does K'hari have?");
 
-
-
-let score=
-
-userScore ++;
-console.log:("userScore:" userScore)
-
-"can you guess how many middle names i have?"
-
+if (guessQuestion === 4 || guessQuestion === "four").toLowerCase(); {
+alert("Somebody was paying attention! K'hari does indeed have" + guessQuestion + "middle names")
+} else {
+  alert("wrong try again");
+}

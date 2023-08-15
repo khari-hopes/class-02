913a04c4483749033cc7a5665c0de2c1d46a7341
Khari Hopes <buildingkhari@yahoo.com>
2023-08-14 20:35:41 -0500

    added first function

--- js/app.js
+++ js/app.js
@@ -1,115 +1,121 @@
-"use strict";
+'use strict';
 
-let playGame = confirm("Do you want to play a game?");
+let playGame = confirm('Do you want to play a game?');
 
 if (playGame) {
   let userScore = 0;
 
-let username = prompt("Hi, What is your name?");
-console.log("User's name", username);
+  let username = prompt('Hi, What is your name?');
+  console.log('Users name', username);
 
-let myBirth = prompt("Was Khari born in Wisconsin?").toLowerCase();
+  let myBirth = prompt('Was Khari born in Wisconsin?').toLowerCase();
 
-if (myBirth === 'yes' || myBirth === 'y') {
-  alert("Good job " + username + ". K'hari was born in Wisconsin");
-  userScore++;
-  console.log("userScore: ", userScore);
-} else {
-  alert("No K'hari was indeed born in Wisconsin");
-}
-
-let middleName = prompt("Does K'hari have four middle names?").toLowerCase();
-   
-if (middleName === 'yes' || middleName === 'y') {
-  userScore++;
-  console.log("userScore: ", userScore);
-alert("Good job " + username + ". K'hari does indeed have 4 middle names");
-} else {
-alert("Sorry K'hari does indeed have 4 middle names");
-}
-
-let myLocation = prompt("Does K'hari live in California?").toLowerCase();
-
-if (myLocation === 'no' || myLocation === 'n') {
-  userScore++;
-  console.log("userScore: ", userScore);
-  alert("Good job " + username + ". K'hari lives in Louisiana!");
+  if (myBirth === 'yes' || myBirth === 'y') {
+    alert('Good job ' + username + '. Khari was born in Wisconsin');
+    userScore++;
+    console.log('userScore: ', userScore);
   } else {
-  alert("Sorry K'hari doesn't live in California");
+    alert('No Khari was indeed born in Wisconsin');
+  }
+function mnQuestion(){
+    let middleName = prompt('Does Khari have four middle names?').toLowerCase();
+    if (middleName === 'yes' || middleName === 'y') {
+      userScore++;
+      console.log('userScore: ', userScore);
+      alert('Good job ' + username + '. Khari does indeed have 4 middle names');
+    } else {
+      alert('Sorry Khari does indeed have 4 middle names');
+    }
+  }
+mnQuestion();
+
+  let myLocation = prompt('Does Khari live in California?').toLowerCase();
+
+  if (myLocation === 'no' || myLocation === 'n') {
+    userScore++;
+    console.log('userScore: ', userScore);
+    alert('Good job ' + username + '. Khari lives in Louisiana!');
+  } else {
+    alert('Sorry Khari does not live in California');
   }
 
-
-let mySiblings = prompt("Does K'hari have seven siblings?").toLowerCase();
-
-if (mySiblings === 'yes' || mySiblings === 'y') {
-  userScore++;
-  console.log("userScore: ", userScore);
-  alert("Good job " + username + ". K'hari does indeed have 7 siblings");
-  } else {
-  alert("Wrong K'hari does have 7 siblings");
-  }
-
-  let cars = prompt("Does K'hari like cars?").toLowerCase();
+  let mySiblings = prompt('Does Khari have seven siblings?').toLowerCase();
 
   if (mySiblings === 'yes' || mySiblings === 'y') {
     userScore++;
-    console.log("userScore: ", userScore);
-    alert("Of course! " + username + ". K'hari loves cars!");
-    } else {
-    alert("Wrong K'hari loves cars!");
-    }
-
-let correctAnswer = 4;
-let userGuess;
-
-while (correctAnswer !== userGuess) {
-  userGuess = prompt("Guess how many middle names does K'hari have?")
-  userGuess = parseInt(userGuess);
-
-  if (userGuess < correctAnswer) {
-    alert("Too low!")
-  } else if (userGuess > correctAnswer) {
-    alert("Too high");
-  } else if (userGuess === correctAnswer) {
-    alert("Somebody was paying attention! K'hari does indeed have " + correctAnswer + " middle names");
-    userScore++;
-  }
-}
-
-let interestsArray = [
-  "Mclaren 720s",
-  "Shelby GT500",
-  "Ford GT",
-  "Audi R8",
-  "Lamborghini Aventador SVJ",
-  "Tesla X Plaid",
-  "Hellcat Durango",
-  "Trackhawk",
-  "AMG GT63 S",
-  "Porsche Turbo S",
-];
-
-let attempts = 3;
-
-hasAttempts: while (attempts !== 0) {
-  attempts--;
-  userGuess = prompt("Can you guess one of my top 10 dream cars?");
-
-  for (let i = 0; i < interestsArray.length; i++) {
-    if (userGuess === interestsArray[i]) {
-      userScore++;
-      alert("Yes! " + interestsArray[i] + " is on the list.");
-      break hasAttempts;
-    }
-  }
-
-  if (attempts > 0) {
-    alert("Wrong, Try again! You have " + attempts + " attempts left.");
+    console.log('userScore: ', userScore);
+    alert('Good job ' + username + '. Khari does indeed have 7 siblings');
   } else {
-    alert("You're out of attempts. The correct answers are: " +
-      interestsArray.join(", "));
+    alert('Wrong Khari does have 7 siblings');
   }
-}
 
-alert("Amazing! Your score is " + userScore);
+  let cars = prompt('Does Khari like cars?').toLowerCase();
+
+  if (mySiblings === 'yes' || mySiblings === 'y') {
+    userScore++;
+    console.log('userScore: ', userScore);
+    alert('Of course! ' + username + '. Khari loves cars!');
+  } else {
+    alert('Wrong Khari loves cars!');
+  }
+
+  let correctAnswer = 4;
+  let userGuess;
+
+  while (correctAnswer !== userGuess) {
+    userGuess = prompt('Guess how many middle names does Khari have?');
+    userGuess = parseInt(userGuess);
+
+    if (userGuess < correctAnswer) {
+      alert('Too low!');
+    } else if (userGuess > correctAnswer) {
+      alert('Too high');
+    } else if (userGuess === correctAnswer) {
+      alert(
+        'Somebody was paying attention! Khari does indeed have ' +
+          correctAnswer +
+          ' middle names'
+      );
+      userScore++;
+    }
+  }
+
+  let interestsArray = [
+    'Mclaren 720s',
+    'Shelby GT500',
+    'Ford GT',
+    'Audi R8',
+    'Lamborghini Aventador SVJ',
+    'Tesla X Plaid',
+    'Hellcat Durango',
+    'Trackhawk',
+    'AMG GT63 S',
+    'Porsche Turbo S',
+  ];
+
+  let attempts = 3;
+
+  hasAttempts: while (attempts !== 0) {
+    attempts--;
+    userGuess = prompt('Can you guess one of my top 10 dream cars?');
+
+    for (let i = 0; i < interestsArray.length; i++) {
+      if (userGuess === interestsArray[i]) {
+        userScore++;
+        alert('Yes! ' + interestsArray[i] + ' is on the list.');
+        break hasAttempts;
+      }
+    }
+
+    if (attempts > 0) {
+      alert('Wrong, Try again! You have ' + attempts + ' attempts left.');
+    } else {
+      alert(
+        'You are out of attempts. The correct answers are: ' +
+          interestsArray.join(', ')
+      );
+    }
+  }
+
+  alert('Amazing! Your score is ' + userScore);
 }
